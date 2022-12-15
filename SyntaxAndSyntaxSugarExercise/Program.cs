f442155dd47019e7c27e2c0943d0e25baee27f04
jose valdez <josezepedavaldez30@gmail.com>
2022-12-14 23:43:42 -0800

    initial commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // inferred typing
+            //int answer = 4;
+            // string response;
+
+            //if (answer < 9)
+            {
+                // response = answer + " is less than nine";
+            }
+            //else
+            {
+                // response = answer + "greater than or equal to nine";
+            }
+
+            //Console.WriteLine(answer);
+           
+            
+            
+            
+            
+            
+            // Ternary Operator
+            //var answer = 4;
+            //var response = (9 > answer) ? " is less than nine " : "  is great than or equal to nine";
+            
+            //Console.WriteLine(answer + response);
+
+            
+            
+            
+            
+            
+            
+            
+            //String interpolation
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+
+            Console.WriteLine( response);
+
+
+
+
         }
     }
 }
